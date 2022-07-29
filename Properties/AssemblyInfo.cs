d64cf92c166a34b746956503afefb3d72d5729d6
Connor <cimok2000@gmail.com>
2022-07-29 19:16:15 +0100

    Add project files.

---
+++ Properties/AssemblyInfo.cs
@@ -0,0 +1,21 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+using MelonLoader;
+
+[assembly: AssemblyTitle(cimokUtils.BuildInfo.Name)]
+[assembly: AssemblyDescription(cimokUtils.BuildInfo.Description)]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany(cimokUtils.BuildInfo.Company)]
+[assembly: AssemblyProduct(cimokUtils.BuildInfo.Name)]
+[assembly: AssemblyCopyright("Copyright ©  2022")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// MelonLoader Stuff
+[assembly: MelonInfo(typeof(cimokUtils.cimokUtilsMod), cimokUtils.BuildInfo.Name, cimokUtils.BuildInfo.Version, cimokUtils.BuildInfo.Author, cimokUtils.BuildInfo.DownloadLink)]
+[assembly: MelonColor()]
+[assembly: MelonGame("Alpha Blend Interactive", "ChilloutVR")]
+[assembly: ComVisible(false)]
+[assembly: Guid("29495c08-e80c-42f3-ba0a-9cf4f63f19b2")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
